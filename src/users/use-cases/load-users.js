947b28b09c0b77d078538165f076b2c6a27a83bf
mikeaude1 <miguelaude@ymail.com>
2024-05-07 22:51:02 -0600

    cambios en ej json

--- src/users/use-cases/load-users.js
+++ src/users/use-cases/load-users.js
@@ -7,7 +7,7 @@ import { User } from "../models/users";
      * @param {Number} page 
      * @returns {Promise<User[]>}
      */
-    export const loadUsersByPage = async ( page=1 ) => { 
+    export const loadUsersByPage = async ( page = 1 ) => { 
 
         const url = `${import.meta.env.VITE_BASE_URL }/users?_page=${ page }`
         const res = await fetch(url);
